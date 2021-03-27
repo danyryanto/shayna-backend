1730c9ad5dabdc4924be1decbb357387daa6c4fd
danyryanto <danyryanto33@gmail.com>
2021-03-27 21:27:40 +0700

    auth admin

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'App\Http\Controllers\DashboardController@index')->name('dashboard');
+
+Route::get('/debug-sentry', function () {
+    throw new Exception('My first Sentry error!');
+});
+
+Auth::routes(['register' => false]);
+
+Route::resource('product', ProductController::class);
